e608af273f7bb9b3d4bd236565d67a47f2e11da0
Tony Pitale <tpitale@gmail.com>
2016-04-03 22:10:54 -0500

    Add creation timestamp

--- lib/form_bridge/endpoint/submissions/create.rb
+++ lib/form_bridge/endpoint/submissions/create.rb
@@ -7,12 +7,15 @@ module FormBridge
         end
 
         def call(request)
+          # ensure we set a creation timestamp for later de-duplication
+          request.params["created_at"] ||= Time.now.to_i
+
           location = request.params["_next"]
           form = form_for(request.params["form_id"])
 
           form.submissions << Submission.from_params(request.params)
           form.save
-          
+
           [302, {"Location" => location}, []]
         end
 
--- lib/form_bridge/version.rb
+++ lib/form_bridge/version.rb
@@ -1,3 +1,3 @@
 module FormBridge
-  VERSION = "0.0.1.alpha3"
+  VERSION = "0.0.1.alpha4"
 end
